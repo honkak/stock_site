a13dffc42c123134bb87f46368c09d4530d9f1bd
honkak <149265734+honkak@users.noreply.github.com>
2024-10-20 00:54:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -64,14 +64,23 @@ for code in codes:
 # 종목 코드와 이름 좌우 배열로 표시
 col_name1, col_name2, col_name3 = st.columns(3)
 
+# with col_name1:
+#     st.write(f"{code1} ({stocks_info.get(code1.strip(), '')})")
+
+# with col_name2:
+#     st.write(f"{code2} ({stocks_info.get(code2.strip(), '')})")
+
+# with col_name3:
+#     st.write(f"{code3} ({stocks_info.get(code3.strip(), '')})")
+
 with col_name1:
-    st.write(f"{code1} ({stocks_info.get(code1.strip(), '')})")
+    st.write(f"({stocks_info.get(code1.strip(), '')})")  # 종목 코드 제거
 
 with col_name2:
-    st.write(f"{code2} ({stocks_info.get(code2.strip(), '')})")
+    st.write(f"({stocks_info.get(code2.strip(), '')})")
 
 with col_name3:
-    st.write(f"{code3} ({stocks_info.get(code3.strip(), '')})")
+    st.write(f"({stocks_info.get(code3.strip(), '')})")
 
 # '시점고정비율' 체크박스
 fixed_ratio = st.checkbox("시점고정비율")
