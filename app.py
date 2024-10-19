be0cf668e72bbb18292754226b7d22b153f841d6
honkak <149265734+honkak@users.noreply.github.com>
2024-10-20 00:57:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -74,13 +74,13 @@ col_name1, col_name2, col_name3 = st.columns(3)
 #     st.write(f"{code3} ({stocks_info.get(code3.strip(), '')})")
 
 with col_name1:
-    st.markdown(f"<span style='color: blue; font-weight: bold;'>({stocks_info.get(code1.strip(), '')})</span>", unsafe_allow_html=True)
+    st.markdown(f"<span style='color: blue;'>({stocks_info.get(code1.strip(), '')})</span>", unsafe_allow_html=True)
 
 with col_name2:
-    st.markdown(f"<span style='color: skyblue; font-weight: bold;'>({stocks_info.get(code2.strip(), '')})</span>", unsafe_allow_html=True)
+    st.markdown(f"<span style='color: skyblue;'>({stocks_info.get(code2.strip(), '')})</span>", unsafe_allow_html=True)
 
 with col_name3:
-    st.markdown(f"<span style='color: red; font-weight: bold;'>({stocks_info.get(code3.strip(), '')})</span>", unsafe_allow_html=True)
+    st.markdown(f"<span style='color: red;'>({stocks_info.get(code3.strip(), '')})</span>", unsafe_allow_html=True)
 
 # '시점고정비율' 체크박스
 fixed_ratio = st.checkbox("시점고정비율")
