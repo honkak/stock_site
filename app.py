6123d6dee4ee46f01fb9e4c20e16429303fee10f
honkak <149265734+honkak@users.noreply.github.com>
2024-10-24 02:18:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -387,40 +387,6 @@ for code in codes:
 
         except Exception as e:
             st.error(f"{code}의 데이터를 가져오는 데 오류가 발생했습니다: {e}")
-
-
-
-
-
-# for code in codes:
-#     if code:
-#         try:
-#             # 한국 종목 코드에 .KS 추가, 지수는 ^ 추가
-#             if code.isdigit() and len(code) == 6:  # 한국 종목 코드
-#                 code = f"{code}.KS"
-#             elif code in index_codes:  # 지수 목록에 있는 경우에는 ^ 추가
-#                 code = f"^{code}"
-#             # 종목의 시작가와 종가 조회
-#             data = yf.download(code, start=start_date, end=end_date)
-#             if not data.empty:
-#                 starting_price = data['Close'].iloc[0]  # 시작가
-#                 ending_price = data['Close'].iloc[-1]   # 종가
-
-#                 # 수익률 및 수익금액 계산
-#                 return_rate = (ending_price - starting_price) / starting_price * 100
-#                 profit_amount = initial_investment * (ending_price / starting_price - 1)
-#                 results.append([code, stocks_info.get(code.strip(), '종목명을 찾을 수 없습니다.'), 
-#                 return_rate, 
-#                 profit_amount])  # 종목명 추가
-#                 results.append([
-#                     code, 
-#                     stocks_info.get(code.strip(), '종목명을 찾을 수 없습니다.'), 
-#                     return_rate, 
-#                     profit_amount
-#                 ])
-
-#         except Exception as e:
-#             st.error(f"{code}의 데이터를 가져오는 데 오류가 발생했습니다: {e}")
             
 
 # 결과 데이터프레임 생성
