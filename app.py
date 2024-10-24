da8614cbf9961ed168c53e0e0b4d45eb6272da36
honkak <149265734+honkak@users.noreply.github.com>
2024-10-24 22:38:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -362,7 +362,7 @@ if "analytics" not in st.experimental_get_query_params():
 
 # 사용자 추적, 결과는 항상 표시되고, 비밀번호는 'qqqq'로 설정
 with streamlit_analytics.track(unsafe_password="qqqq"):
-    st.subheader("다빈치 차트 App with Analytics")
+    # st.subheader("다빈치 차트 App with Analytics")
     
     # 사용자 상호작용을 추적할 수 있는 위젯들
     # st.text_input("Write something")
