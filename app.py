f2b4fe80f17cdf441d11d538c21f904ff9918ead
honkak <149265734+honkak@users.noreply.github.com>
2024-10-25 00:02:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -366,17 +366,35 @@ with streamlit_analytics.track(
 ):
     
     # Analytics Dashboard 글자 크기 조정 및 설명 텍스트 숨기기
+    # st.markdown("""
+    #     <style>
+    #     /* Analytics Dashboard 크기 조정 */
+    #     div[data-testid="stMarkdownContainer"] h1 {
+    #         font-size: 18px !important;
+    #         text-align: center;
+    #     }
+    #     # /* 분석 대시보드의 특정 설명 텍스트 숨기기 */
+    #     div[data-testid="stMarkdownContainer"] p {
+    #         display: none;
+    #     }
+    #     </style>
+    # """, unsafe_allow_html=True)
     st.markdown("""
-        <style>
-        /* Analytics Dashboard 크기 조정 */
-        div[data-testid="stMarkdownContainer"] h1 {
-            font-size: 18px !important;
-            text-align: center;
-        }
-        # /* 분석 대시보드의 특정 설명 텍스트 숨기기 */
-        div[data-testid="stMarkdownContainer"] p {
-            display: none;
-        }
-        </style>
-    """, unsafe_allow_html=True)
-    
+    <style>
+    /* Analytics Dashboard 크기 조정 */
+    div[data-testid="stMarkdownContainer"] h1 {
+        font-size: 18px !important;
+        text-align: center;
+    }
+    /* 분석 대시보드의 특정 설명 텍스트 숨기기 */
+    div[data-testid="stMarkdownContainer"] p {
+        display: none;
+    }
+    /* "Psst!" 메시지 숨기기 */
+    div[data-testid="stMarkdownContainer"] div:nth-child(2) {
+        display: none;
+    }
+    </style>
+""", unsafe_allow_html=True)
+
+
