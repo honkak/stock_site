c150890e2ac5b3548a76afc6bff5cae6678b8178
honkak <149265734+honkak@users.noreply.github.com>
2024-11-05 00:06:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -360,7 +360,7 @@ with streamlit_analytics.track(unsafe_password="qqqq"):
     <style>
     /* Analytics Dashboard 크기 조정 */
     div[data-testid="stMarkdownContainer"] h1 {
-        font-size: 18px !important;
+        font-size: 24px !important;
         text-align: center;
     }
     </style>
